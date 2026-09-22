c59253764dab57f8bbb5ad93270babea139a5808
Adrian Szuter <adrsz812@student.liu.se>
2026-09-22 22:03:11 +0200

    feat: add resize functions to webgpu-utils

--- src/webgpu-utils.ts
+++ src/webgpu-utils.ts
@@ -35,3 +35,47 @@ export function createBuffer(
   device.queue.writeBuffer(buffer, 0, data);
   return buffer;
 }
+
+// Best version for precision. However, it clashes with rAF loops since it async
+export function observeCanvasResize(
+  canvas: HTMLCanvasElement,
+  device: GPUDevice,
+  onResize: () => void
+): { disconnect: () => void } {
+  const dpr = Math.min(window.devicePixelRatio, 2) || 1;
+
+  const observer = new ResizeObserver((entries) => {
+    for (const entry of entries) {
+      const width = entry.devicePixelContentBoxSize?.[0].inlineSize || entry.contentBoxSize[0].inlineSize * dpr;
+
+      const height = entry.devicePixelContentBoxSize?.[0].blockSize || entry.contentBoxSize[0].blockSize * dpr;
+
+      const target = entry.target as HTMLCanvasElement;
+      target.width = Math.max(1, Math.min(width, device.limits.maxTextureDimension2D));
+      target.height = Math.max(1, Math.min(height, device.limits.maxTextureDimension2D));
+    }
+
+    onResize();
+  });
+
+  try {
+    observer.observe(canvas, { box: "device-pixel-content-box" });
+  } catch {
+    observer.observe(canvas, { box: "content-box" });
+  }
+
+  return {
+    disconnect: () => observer.disconnect(),
+  };
+}
+
+export function resizeCanvas(canvas: HTMLCanvasElement, device: GPUDevice) {
+  const dpr = Math.min(window.devicePixelRatio, 2) || 1;
+  const width = Math.max(1, Math.min(canvas.clientWidth * dpr, device.limits.maxTextureDimension2D));
+  const height = Math.max(1, Math.min(canvas.clientHeight * dpr, device.limits.maxTextureDimension2D));
+
+  if (canvas.width !== width || canvas.height !== height) {
+    canvas.width = width;
+    canvas.height = height;
+  }
+}
